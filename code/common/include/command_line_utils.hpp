2be2541e5c69921651becdb8a659f54d599bbffd
Dariusz Walczak <dariusz.walczak@gmail.com>
2025-04-13 10:21:51 +0200

    Added the 'validate_existing_dir_path' function

--- code/common/include/command_line_utils.hpp
+++ code/common/include/command_line_utils.hpp
@@ -1,8 +1,20 @@
 #if !defined COMMON__COMMAND_LINE_UTILS_HPP
 #define COMMON__COMMAND_LINE_UTILS_HPP
 
+#include <filesystem>
 #include <string>
 
+#include <fmt/format.h>
+
+
 std::string validate_person_local_name(const std::string& raw);
+std::string validate_existing_dir_path(const std::string& raw);
+
+/** @brief The fmt library formatter for the std::filesystem::path */
+template <> struct fmt::formatter<std::filesystem::path>: formatter<string_view>
+{
+    auto format(std::filesystem::path p, format_context& ctx) const
+    -> format_context::iterator;
+};
 
 #endif // !defined COMMON__COMMAND_LINE_UTILS_HPP
--- code/common/src/command_line_utils.cpp
+++ code/common/src/command_line_utils.cpp
@@ -13,3 +13,34 @@ std::string validate_person_local_name(const std::string& raw) {
         return "";
     }
 }
+
+
+std::string validate_existing_dir_path(const std::string& raw) {
+    auto path = std::filesystem::path(raw);
+
+
+    bool path_exists = { false };
+
+    try
+    {
+        path_exists = std::filesystem::exists(path);
+    }
+    catch (std::filesystem::filesystem_error e)
+    {
+        return e.what();
+    }
+
+    if (!path_exists)
+    {
+        return fmt::format("the path doesn't exist [{}]", path);
+    }
+    return "";
+}
+
+
+auto fmt::formatter<std::filesystem::path>::format(
+    std::filesystem::path p, format_context& ctx) const
+    -> format_context::iterator
+{
+    return formatter<string_view>::format(p.string(), ctx);
+}
