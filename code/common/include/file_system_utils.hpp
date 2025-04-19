501697574acb79cb074c7819d82c376fcf86d6c7
Dariusz Walczak <dariusz.walczak@gmail.com>
2025-04-19 18:31:49 +0200

    Improved the 'find_input_files' function. Added the 'adapt_string_paths' and 'merge_input_files' functions

--- code/common/include/file_system_utils.hpp
+++ code/common/include/file_system_utils.hpp
@@ -3,12 +3,18 @@
 
 #include <filesystem>
 #include <string>
+#include <set>
 #include <vector>
 
 
-std::vector<std::filesystem::path> find_input_files(
+typedef std::set<std::filesystem::path> input_files;
+
+input_files find_input_files(
     const std::filesystem::path& root_dir_path,
     const std::string& extension);
 
+input_files adapt_string_paths(std::vector<std::string> paths);
+
+input_files merge_input_files(input_files first, input_files second);
 
 #endif // !defined COMMON__FILE_SYSTEM_UTILS_HPP
--- code/common/src/file_system_utils.cpp
+++ code/common/src/file_system_utils.cpp
@@ -1,19 +1,41 @@
 #include "file_system_utils.hpp"
 
+#include <spdlog/spdlog.h>
 
-std::vector<std::filesystem::path> find_input_files(
+
+input_files find_input_files(
     const std::filesystem::path& root_dir_path,
     const std::string& extension)
 {
-    std::vector<std::filesystem::path> result;
+    input_files result;
 
     for (const auto entry : std::filesystem::recursive_directory_iterator(root_dir_path))
     {
-        if (entry.exists() && (entry.is_regular_file() || entry.is_symlink()))
+        if (entry.exists() && entry.is_regular_file())
         {
-            result.push_back(entry.path());
+            if (entry.path().extension().string() == extension) {
+                result.insert(std::filesystem::absolute(entry.path()).lexically_normal());
+            }
         }
     }
 
     return result;
+}
+
+input_files adapt_string_paths(std::vector<std::string> paths)
+{
+    input_files result;
+
+    for (const auto path : paths)
+    {
+        result.insert(std::filesystem::absolute(std::filesystem::path(path)).lexically_normal());
+    }
+
+    return result;
+}
+
+input_files merge_input_files(input_files first, input_files second)
+{
+    first.merge(second);
+    return first;
 }
