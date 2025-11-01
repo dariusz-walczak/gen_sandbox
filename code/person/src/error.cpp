75a2c27ad139b93a2d2d304d923c25c6f5a814b5
Dariusz Walczak <52116076+dariusz-walczak@users.noreply.github.com>
2025-11-01 22:47:33 +0100

    gen_person: Small person_exception tweaks

--- code/person/src/error.cpp
+++ code/person/src/error.cpp
@@ -5,6 +5,13 @@
 namespace person
 {
 
+person_exception::person_exception()
+    : std::exception(), _code(error_code::uninitialized)
+{
+    set_what(_code, nullptr);
+}
+
+
 person_exception::person_exception(error_code code, const std::string& msg)
     : std::exception(), _code(code)
 {
@@ -19,24 +26,32 @@ person_exception::person_exception(error_code code, const char* msg)
 }
 
 
-void person_exception::set_what(error_code code, const char* msg) {
+void person_exception::set_what(error_code code, const char* msg)
+{
     std::ostringstream oss;
 
-    switch (code) {
+    switch (code)
+    {
     case error_code::uninitialized:
         oss << "<uninitialized>";
         break;
-    case error_code::query_error:
-        oss << "<query error>";
-        break;
     case error_code::resource_not_found:
-        oss << "<not found>";
+        oss << "<resource not found>";
+        break;
+    case error_code::multiple_resources_found:
+        oss << "<multiple resources found>";
+        break;
+    case error_code::input_contract_error:
+        oss << "<input contract error>";
         break;
     default:
         oss << "<invalid code>";
     }
 
-    oss << " " << msg;
+    if (msg && *msg)
+    {
+        oss << " " << msg;
+    }
     _what = oss.str();
 }
 
