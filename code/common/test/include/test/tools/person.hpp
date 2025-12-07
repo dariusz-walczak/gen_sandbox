523150a1a40bcc6e768fcb649f036c52c7c043f4
Dariusz Walczak <dariusz.walczak@gmail.com>
2025-12-07 12:15:05 +0100

    OWL-106: Added the `ComparablePerson` helper struct

--- code/common/test/include/test/tools/person.hpp
+++ code/common/test/include/test/tools/person.hpp
@@ -15,6 +15,26 @@ common::Person construct_named_person_val(
 std::shared_ptr<common::Person> construct_named_person_ptr(
     const std::string& uri, const std::string& first_name, const std::string& last_name);
 
+
+struct ComparablePerson
+{
+    std::string id;
+    std::string caption;
+};
+
+inline bool operator==(const ComparablePerson& lhs, const ComparablePerson& rhs)
+{
+    return ((lhs.id == rhs.id) && (lhs.caption == rhs.caption));
+}
+
+[[nodiscard]] ComparablePerson to_comparable(const common::Person& person);
+[[nodiscard]] std::string to_string(const ComparablePerson& person, std::uint8_t depth=0) noexcept;
+
+inline void PrintTo(const ComparablePerson& person, std::ostream* os)
+{
+    *os << to_string(person);
+}
+
 } // namespace test::tools
 
 #endif // !defined TEST_TOOLS_PERSON_HPP
--- code/common/test/src/tools/person.cpp
+++ code/common/test/src/tools/person.cpp
@@ -1,5 +1,7 @@
 #include "test/tools/person.hpp"
 
+#include "test/tools/string.hpp"
+
 namespace test::tools
 {
 
@@ -17,6 +19,22 @@ std::shared_ptr<common::Person> construct_named_person_ptr(
 {
     return std::make_shared<common::Person>(
         construct_named_person_val(uri, first_name, last_name));
+}
+
+ComparablePerson to_comparable(const common::Person& person)
+{
+    return ComparablePerson{person.get_uri_str(), person.get_caption()};
+}
+
+std::string to_string(const ComparablePerson& person, std::uint8_t depth) noexcept
+{
+    const std::string indent = construct_indent(depth);
+
+    std::ostringstream oss;
+    oss << indent << "ComparablePerson{"
+        << "id=" << person.id << ", "
+        << "caption=" << person.caption << "}";
+    return oss.str();
 }
 
 } // namespace test::tools
