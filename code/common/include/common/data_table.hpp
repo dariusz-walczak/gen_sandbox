fe9b712a7cc0fdc210d13fd96881194035cdc70a
Dariusz Walczak <dariusz.walczak@gmail.com>
2026-01-03 10:02:52 +0100

    OWL-92: Added the `get_boolean_value_req` utility function

--- code/common/include/common/data_table.hpp
+++ code/common/include/common/data_table.hpp
@@ -13,6 +13,8 @@ namespace common
 std::vector<std::string> extract_resource_uri_seq(
     const common::data_table& data_table, const std::string& binding_name);
 
+bool get_boolean_value_req(const data_row& row, const std::string& binding_name);
+
 } // namespace common
 
 #endif // !defined COMMON_DATA_TABLE_HPP
--- code/common/test/src/data_table.cpp
+++ code/common/test/src/data_table.cpp
@@ -174,7 +174,7 @@ INSTANTIATE_TEST_SUITE_P(
 
 class DataTable_ExtractResourceUriSeq_MissingBinding : public ::testing::TestWithParam<Param> {};
 
-TEST_P(DataTable_ExtractResourceUriSeq_MissingBinding, MissingBindingErrorCases)
+TEST_P(DataTable_ExtractResourceUriSeq_MissingBinding, BindingNotFoundErrorCase)
 {
     const Param& param = GetParam();
 
@@ -252,3 +252,122 @@ INSTANTIATE_TEST_SUITE_P(
     tools::ParamNameGen<Param>);
 
 } // namespace test::suite_extract_resource_uri_seq
+
+//  The get_boolean_value_req function tests
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+namespace test::suite_get_boolean_value_req
+{
+
+struct ValidInputParam
+{
+    const char* case_name;
+    const common::data_row input_data_row;
+    const std::string input_binding_name;
+
+    const bool expected_value;
+};
+
+class DataTable_GetBooleanValueReq_ValidInput
+    : public ::testing::TestWithParam<ValidInputParam> {};
+
+TEST_P(DataTable_GetBooleanValueReq_ValidInput, NormalSuccessCases)
+{
+    const ValidInputParam& param = GetParam();    
+
+    const auto actual_value =
+        common::get_boolean_value_req(param.input_data_row, param.input_binding_name);
+
+    EXPECT_EQ(actual_value, param.expected_value);
+}
+
+const std::vector<ValidInputParam> g_normal_success_cases_params{
+    {
+        .case_name="True",
+        .input_data_row={{"flag1", "true"}, {"flag2", "false"}, {"name", "ums7"}},
+        .input_binding_name="flag1",
+        .expected_value=true
+    },
+    {
+        .case_name="False",
+        .input_data_row={{"flag1", "false"}, {"flag2", "true"}, {"name", "y6gg"}},
+        .input_binding_name="flag1",
+        .expected_value=false
+    }
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    ,
+    DataTable_GetBooleanValueReq_ValidInput,
+    ::testing::ValuesIn(g_normal_success_cases_params),
+    tools::ParamNameGen<ValidInputParam>);
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
+
+struct InvalidInputParam
+{
+    const char* case_name;
+    const common::data_row input_data_row;
+    const std::string input_binding_name;
+
+    const common::common_exception::error_code expected_error_code;
+};
+
+class DataTable_GetBooleanValueReq_InvalidInput
+    : public ::testing::TestWithParam<InvalidInputParam> {};
+
+TEST_P(DataTable_GetBooleanValueReq_InvalidInput, ErrorCases)
+{
+    const InvalidInputParam& param = GetParam();    
+
+    EXPECT_THROW_WITH_CODE(
+        common::get_boolean_value_req(param.input_data_row, param.input_binding_name),
+        common::common_exception, param.expected_error_code);
+}
+
+const std::vector<InvalidInputParam> g_error_cases_params{
+    {
+        .case_name="BindingNotFound_EmptyInput",
+        .input_data_row={},
+        .input_binding_name="some_flag",
+        .expected_error_code=common::common_exception::error_code::binding_not_found
+    },
+    {
+        .case_name="BindingNotFound_NameMismatch",
+        .input_data_row={{"iu5l", "j98p"}, {"flag", "false"}, {"ohd2", "3h46"}},
+        .input_binding_name="mismatched_flag_name",
+        .expected_error_code=common::common_exception::error_code::binding_not_found
+    },
+    {
+        .case_name="InputContractError_UppercaseTrue",
+        .input_data_row={{"flag", "TRUE"}, {"value", "0x75"}},
+        .input_binding_name="flag",
+        .expected_error_code=common::common_exception::error_code::input_contract_error
+    },
+    {
+        .case_name="InputContractError_CapitalizedFalse",
+        .input_data_row={{"flag", "False"}, {"value", "0x61"}},
+        .input_binding_name="flag",
+        .expected_error_code=common::common_exception::error_code::input_contract_error
+    },
+    {
+        .case_name="InputContractError_Zero",
+        .input_data_row={{"flag", "0"}, {"value", "0xC1"}},
+        .input_binding_name="flag",
+        .expected_error_code=common::common_exception::error_code::input_contract_error
+    },
+    {
+        .case_name="InputContractError_RandomString",
+        .input_data_row={{"flag", "v36zh4gh2f5m"}, {"value", "0xEC"}},
+        .input_binding_name="flag",
+        .expected_error_code=common::common_exception::error_code::input_contract_error
+    }
+};
+
+INSTANTIATE_TEST_SUITE_P(
+    ,
+    DataTable_GetBooleanValueReq_InvalidInput,
+    ::testing::ValuesIn(g_error_cases_params),
+    tools::ParamNameGen<InvalidInputParam>);
+
+} // namespace test::suite_get_boolean_value_req
