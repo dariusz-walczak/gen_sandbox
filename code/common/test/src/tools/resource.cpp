fdc95f0dbb60b01e6f173006686420a8cfa59643
Dariusz Walczak <dariusz.walczak@gmail.com>
2026-02-14 21:39:26 +0100

    OWL-92: Added `to_comparable` test utility functions for a single `common::Resource` object as well as a sequence of these objects

--- code/common/test/src/tools/resource.cpp
+++ code/common/test/src/tools/resource.cpp
@@ -9,6 +9,25 @@
 namespace test::tools
 {
 
+ComparableResource to_comparable(const common::Resource& resource)
+{
+    return ComparableResource{resource.get_uri_str(), resource.get_caption()};
+}
+
+std::vector<ComparableResource> to_comparable(
+    const std::vector<std::shared_ptr<common::Resource>>& resource_seq)
+{
+    std::vector<ComparableResource> output;
+    output.reserve(resource_seq.size());
+
+    for (const auto& resource : resource_seq)
+    {
+        output.emplace_back(to_comparable(*resource));
+    }
+
+    return output;
+}
+
 std::string to_string(const ComparableResource& res, std::uint8_t depth) noexcept
 {
     const std::string indent = construct_indent(depth);
