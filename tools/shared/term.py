3f5847cd8cf818cf1d70660150862839f1887c63
Dariusz Walczak <dariusz.walczak@gmail.com>
2026-08-29 12:26:37 +0200

    OWL-141: Other small fixes

--- tools/shared/term.py
+++ tools/shared/term.py
@@ -9,7 +9,7 @@ import pydantic
 
 # NOTE: Keep the TERM_NAME_SINGLELINE_PATTERN_STRING and the TERM_NAME_MULTILINE_PATTERN_STRING
 #       constants consistent in terms of all non-whitespace characters.
-#       When the pattern changes, update it copy in the docs/conventions/terms-and-definitions.md
+#       When the pattern changes, update its copy in the docs/conventions/terms-and-definitions.md
 #       document (search for 'Python re pattern'):
 TERM_NAME_SINGLELINE_PATTERN_STRING = r"[\w \-().:,!?.]+"
 TERM_NAME_MULTILINE_PATTERN_STRING = r"[\w\s\-().:,!?.]+"
